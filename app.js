cb4e3a963ce6d184456b39cb3625e86e9e7c096e
Soichiro-Yoshimura <soichiro_yoshimura@dwango.co.jp>
2015-12-07 08:34:56 +0000

    ファイルを読み込むところまで

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 (function () {
 	'use strict';
+	let fs = require('fs');
+	let readline = require('readline');
+	let rs = fs.ReadStream('./popu-pref.csv');
+	let rl = readline.createInterface({ 'input': rs, 'output': {} });
 	
+	rl.on('line', (line) => {
+		console.log(line);
+	});
+	rl.resume();
 })();
